d3760f465d3147c5c4bea3440f6f475db5038434
Mohammed Terry-Jack <mohammedterryjack@gmail.com>
2020-07-08 15:50:00 +0100

    renaming

--- data_analysis/utils.py
+++ data_analysis/utils.py
@@ -1,11 +1,12 @@
 ############   NATIVE IMPORTS  ###########################
+from typing import Dict, Set
 ############ INSTALLED IMPORTS ###########################
-from pandas import read_csv
+from pandas import read_csv, concat, DataFrame
 from sklearn.feature_extraction.text import CountVectorizer
 ############   LOCAL IMPORTS   ###########################
 ##########################################################
 class QuranArabicGrammarCSVHeaders:
-    _PATH = "../data/{filename}"
+    _PATH = "../raw_data/{filename}"
     _FILENAME = "quran_arabic_grammar.txt"
     _DELIMITER = "\t"
     WORD_INDEX = "LOCATION"
@@ -16,13 +17,16 @@ class QuranArabicGrammarCSVHeaders:
     _LEMMA_PREFIX = "LEM:"
     _ROOT_PREFIX = "ROOT:"
 
-def analyse_quran_arabic_grammar_file() -> None:
+def analyse_quran_arabic_grammar_file() -> DataFrame:
     quran = read_csv(
         filepath_or_buffer=QuranArabicGrammarCSVHeaders._PATH.format(
             filename=QuranArabicGrammarCSVHeaders._FILENAME
         ), 
         sep=QuranArabicGrammarCSVHeaders._DELIMITER, 
         header=0
+    )
+    pos_tags = quran[QuranArabicGrammarCSVHeaders.POS_TAG].apply(
+        lambda pos_tag:f"POS:{pos_tag}"
     )
     features = quran[QuranArabicGrammarCSVHeaders.FEATURES].apply(
         lambda features_as_string:features_as_string.split(QuranArabicGrammarCSVHeaders._FEATURE_DELIMITER)
@@ -48,8 +52,6 @@ def analyse_quran_arabic_grammar_file() -> None:
             lowercase=False
         ).fit([root]).get_feature_names() if root else []
     )
-    print(root_ngrams)
-
     indexes = quran[QuranArabicGrammarCSVHeaders.WORD_INDEX].apply(
         lambda index_as_string:list(map(int,index_as_string.strip("()").split(":")))
     )
@@ -59,10 +61,66 @@ def analyse_quran_arabic_grammar_file() -> None:
     verses = indexes.apply(
         lambda index:index[1]
     )
+    return concat(
+        objs=[
+            chapters,
+            verses,
+            pos_tags,
+            words,
+            root_ngrams
+        ],
+        keys=[
+            "CHAPTER",
+            "VERSE",
+            "PART OF SPEECH",
+            "WORD",
+            "ROOT N-GRAMS"
+        ],
+        axis=1
+    )
 
-    #print(chapters)
-    #print(verses)
-    #print(words)
-    #print("vocab size = ",len(set(words)))
+def generate_arabic_feature_set(arabic_features:DataFrame) -> dict:
+    vector_features = {}
 
-analyse_quran_arabic_grammar_file()
+    for chapter,verse,pos,word,ngrams in arabic_features.itertuples(index=False):  
+        vector_key = f"{chapter}:{verse}"
+        if vector_key not in vector_features:
+            vector_features[vector_key] = set()
+
+        vector_features[vector_key].add(word)
+        vector_features[vector_key].add(pos)
+        vector_features[vector_key] |= set(ngrams)
+
+    return vector_features
+
+def similarity_of_two_sets_of_features(features_a:set, features_b:set) -> float:
+    features_in_common = features_a.intersection(features_b)
+    features_in_total = features_a | features_b
+    return len(features_in_common) / len(features_in_total)
+
+def save_searchable_quran_to_file(filename:str, arabic_feature_sets:Dict[str,Set[str]]) -> None:
+    data = DataFrame(
+        arabic_feature_sets.items(),
+        columns = ["VERSE","FEATURE"]
+    )
+    data["VERSE CROSS-REFERENCE SIMILARITY"] = data["FEATURE"].apply(
+        lambda feature_set_a: list(
+            map(
+                lambda feature_set_b: similarity_of_two_sets_of_features(
+                    features_a=feature_set_b,
+                    features_b=feature_set_a
+                ),
+                arabic_feature_sets.values()
+        )
+    )
+    data = data.set_index('VERSE')
+    data = data.drop(columns=["FEATURE"])
+    data.to_csv(filename)
+
+
+save_searchable_quran_to_file(
+    arabic_feature_sets = generate_arabic_feature_set(
+        arabic_features=analyse_quran_arabic_grammar_file()
+    ),
+    filename="../data/quran"
+)
