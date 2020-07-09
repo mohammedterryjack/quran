0d7cd4f12dfa674c3aebfe97032ecbc17a23ba10
Mohammed Terry-Jack <mohammedterryjack@gmail.com>
2020-07-09 22:25:41 +0100

    english quran now saved as json

--- app/test.py
+++ app/test.py
@@ -1,10 +1,11 @@
 ############   NATIVE IMPORTS  ###########################
 ############ INSTALLED IMPORTS ###########################
-from pandas import read_csv
+from pandas import read_csv, read_json
 ############   LOCAL IMPORTS   ###########################
 ##########################################################
 
-quran = read_csv("data/quran_cross_referenced.csv", sep="\t")
+quran = read_csv("data/quran.csv", sep="\t")
+quran_en = read_json("data/quran_en.json")
 
 while True:
     translator = max(0,min(17,int(input("\nEnter a number between 0 and 17: "))))
@@ -14,7 +15,6 @@ while True:
     example_cross_references = example_cross_references.to_list()[0].strip("[]").split(",")
     for cross_reference in example_cross_references:
         verse_name = cross_reference.lstrip(" '").rstrip("'")
-        translations = quran["ENGLISH"][quran["VERSE"]==verse_name].to_list()[0].split("\n")
-        example_translation = translations[translator].lstrip(f"{translator}").strip()
+        translations = quran_en[verse_name]["ENGLISH"]
+        example_translation = translations[translator]
         print(f"{verse_name} = {example_translation}")
-
