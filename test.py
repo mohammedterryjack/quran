13ad7b177e0da0e2e5dd6892ed476a15b43514c7
Mohammed Terry-Jack <mohammedterryjack@gmail.com>
2020-07-09 23:38:12 +0100

    search done

--- test.py
+++ test.py
@@ -11,8 +11,8 @@ from data_analysis.utils import similarity_of_two_sets_of_features
 quran = read_json("data/quran.json")
 quran_en = read_json("data/quran_en.json")
 
-def english_translation_of_verse(verse:str,quran_en:dict,translator:int=0) -> str:
-    return quran_en[verse]["ENGLISH"][max(min(translator,18),0)]
+def english_translation_of_verse(verse:str,quran_en:dict,translator:int=5) -> str:
+    return quran_en[verse]["ENGLISH"][max(min(translator,17),0)]
 
 def similar_verses_to_verse(verse:str, quran:dict, top_n:int=3) -> List[str]:
     return quran[verse]["CROSS-REFERENCE"][:max(min(top_n,10),0)]
@@ -28,6 +28,6 @@ def similar_verses_to_query(query:str,quran:dict,quran_en:dict,top_n:int=3) -> L
     most_similar_indexes = argsort(semantic_similarity_scores.to_list())[:-top_n-1:-1]
     print(most_similar_indexes)
 
-verse = "1:1"
+verse = "114:6"
 print(english_translation_of_verse(verse=verse,quran_en=quran_en))
 print(similar_verses_to_verse(verse=verse,quran=quran))
