d87b5b9c045821943502cd22aac94094ce335a17
Mohammed Terry-Jack <mohammedterryjack@gmail.com>
2020-07-10 15:17:00 +0100

    testing

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 ############   NATIVE IMPORTS  ###########################
-from typing import List,Iterable,Set,Optional
-from difflib import get_close_matches
+from typing import List,Iterable,Set,Optional,Tuple
+from difflib import SequenceMatcher
+from json import load
 ############ INSTALLED IMPORTS ###########################
 from pandas import read_json
+from numpy import argsort
 ############   LOCAL IMPORTS   ###########################
 from data_analysis.semantic_featuriser import (
     set_of_semantic_features_for_sentence,
@@ -12,12 +14,13 @@ from data_analysis.semantic_featuriser import (
 ##########################################################
 
 quran = read_json("data/quran.json")
-quran_features = read_json("data/quran_features.json")
 quran_en = read_json("data/quran_en.json")
+with open("data/quran_features.json") as json_file:
+    quran_features = load(json_file)
 
 def semantic_features(quran_features:dict) -> Iterable[Set[str]]:
     for verse in quran_features:
-        yield set(quran_features[verse]["SEMANTIC FEATURES"])
+        yield set(quran_features[verse])
 
 def list_english_translations_for_all_verses(quran_en:dict) -> Iterable[Tuple[str,str]]:
     for verse in quran_en:
@@ -29,10 +32,9 @@ def english_translation_of_verse(verse:str,quran_en:dict,translator:Optional[int
     return parallel_translations_of_verse[max(min(translator,17),0)] if translator else parallel_translations_of_verse
 
 def similar_verses_to_verse(verse:str, quran:dict, top_n:int=3) -> List[str]:
-    return quran[verse]["CROSS-REFERENCE"][:max(min(top_n,10),0)]
+    return quran[verse][:max(min(top_n,10),0)]
 
-def most_semantically_similar_verse_to_query(query:str,quran_features:dict) -> str:
-    verse_names = list(quran_features.keys())
+def most_semantically_similar_verse_to_query(query:str,quran_features:dict,verse_names:List[str]) -> str:
     query_features = set_of_semantic_features_for_sentence(query)
     semantic_scores = list(
         map(
@@ -47,16 +49,36 @@ def most_semantically_similar_verse_to_query(query:str,quran_features:dict) -> s
     verse_index = semantic_scores.index(best_score)
     return verse_names[verse_index]
 
-def most_syntactically_similar_verse_to_query(query:str,quran_en:dict) -> str:
-    verses,translations = list(list_english_translations_for_all_verses(quran_en))
-    print(verses)
-
-    #similar_verse = get_close_matches(word=query,possibilities=)
+def syntactically_similar_verse_to_query(query:str,quran_en:dict) -> List[str]:
+    verses,translations = zip(*list_english_translations_for_all_verses(quran_en))
+    similarity_scores = list(
+        map(
+            lambda translation:SequenceMatcher(None, query, translation).ratio(),
+            translations
+        )
+    )
+    best_indexes = argsort(similarity_scores)[:-4:-1]
+    best_verses = list(map(lambda index:verses[index], best_indexes))
+    best_verses_no_duplicates = []
+    for verse in best_verses:
+        if verse not in best_verses_no_duplicates:
+            best_verses_no_duplicates.append(verse)
+    return best_verses_no_duplicates
 
 while True:
     query = input(">")
-    most_syntactically_similar_verse_to_query(query=query,quran=quran_en)
-    verse = most_semantically_similar_verse_to_query(query=query,quran_features=quran_features)
+    # print("FUZZY")
+    # verses = syntactically_similar_verse_to_query(query=query,quran_en=quran_en)
+    # for verse in verses:
+    #     print(verse)
+    #     print(english_translation_of_verse(verse=verse,quran_en=quran_en))
+    #     print()
+    # print("MEANING")
+    verse = most_semantically_similar_verse_to_query(
+        query=query,
+        quran_features=quran_features,
+        verse_names = list(quran_en.keys())
+    )
     related_verses = similar_verses_to_verse(verse=verse,quran=quran)
     for verse in related_verses:
         print(verse)
