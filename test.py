43edd950464079799426b80d7edfbe829ae9a956
Mohammed Terry-Jack <mohammedterryjack@gmail.com>
2020-07-10 11:42:49 +0100

    test complete

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 ############   NATIVE IMPORTS  ###########################
-from typing import List,Iterable,Set
+from typing import List,Iterable,Set,Optional
 ############ INSTALLED IMPORTS ###########################
 from pandas import read_json
 from numpy import argsort
@@ -17,8 +17,9 @@ def semantic_features(quran:dict) -> Iterable[Set[str]]:
     for verse in quran:
         yield set(quran[verse]["SEMANTIC FEATURES"])
 
-def english_translation_of_verse(verse:str,quran_en:dict,translator:int=6) -> str:
-    return quran_en[verse]["ENGLISH"][max(min(translator,17),0)]
+def english_translation_of_verse(verse:str,quran_en:dict,translator:Optional[int]=6) -> str:
+    parallel_translations_of_verse = quran_en[verse]["ENGLISH"]
+    return parallel_translations_of_verse[max(min(translator,17),0)] if translator else parallel_translations_of_verse
 
 def similar_verses_to_verse(verse:str, quran:dict, top_n:int=3) -> List[str]:
     return quran[verse]["CROSS-REFERENCE"][:max(min(top_n,10),0)]
@@ -43,12 +44,13 @@ def similar_verses_to_query(query:str,quran:dict,top_n:int=3) -> List[str]:
         )
     )
 
+
 while True:
     query = input(">")
     similar_verses = similar_verses_to_query(query=query,quran=quran, top_n=2)
     for verse in similar_verses:
         related_verses = similar_verses_to_verse(verse=verse,quran=quran)
         for verse in related_verses:
-            print(verse)
-            print(english_translation_of_verse(verse=verse,quran_en=quran_en))
+            print("\t",verse)
+            print("\t",english_translation_of_verse(verse=verse,quran_en=quran_en))
             print()
