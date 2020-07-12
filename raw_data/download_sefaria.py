072269e0c421ce0b37b148875225b24f99eac70e
Mohammed Terry-Jack <mohammedterryjack@gmail.com>
2020-07-12 21:06:35 +0100

    data files from sefaria

--- raw_data/download_sefaria.py
+++ raw_data/download_sefaria.py
@@ -3,24 +3,24 @@ from os import system
 ############ INSTALLED IMPORTS ###########################
 ############   LOCAL IMPORTS   ###########################
 ##########################################################
-PATH_TO_SAVE = "../data/tanakh/{cannon}/{book}_{language_code}.json"
+PATH_TO_SAVE = "data/tanakh/{cannon}/{book}_{language_code}.json"
 LANGUAGES = ("English","Hebrew")
 CANNON_BOOKS_MAP = {
     "Torah":["Deuteronomy","Exodus","Genesis","Leviticus","Numbers"],
     "Prophets":[
         "Amos","Ezekiel","Habakkuk","Haggai","Hosea",
-        "I Kings","I Samuel","II Kings","II Samuel",
+        "I%20Kings","I%20Samuel","II%20Kings","II%20Samuel",
         "Isaiah","Jeremiah","Joel","Jonah","Joshua",
         "Judges","Malachi","Micah","Nahum","Obadiah",
         "Zechariah","Zephaniah"
     ],
     "Writings":[
         "Daniel","Ecclesiastes","Esther","Ezra",
-        "I Chronicles","II Chronicles","Job",
+        "I%20Chronicles","II%20Chronicles","Job",
         "Lamentations","Nehemiah","Proverbs",
-        "Psalms","Ruth","Song of Songs"
+        "Psalms","Ruth","Song%20of%20Songs"
     ]
-)
+}
 URL = "https://raw.githubusercontent.com/Sefaria/Sefaria-Export/master/json/Tanakh/{cannon}/{book}/{language}/merged.json"
 
 for cannon,books in CANNON_BOOKS_MAP.items():
@@ -34,6 +34,6 @@ for cannon,books in CANNON_BOOKS_MAP.items():
             path_to_save = PATH_TO_SAVE.format(
                 cannon=cannon.lower(),
                 book=book.lower(),
-                language_code = language[:2]
+                language_code = language[:2].lower()
             )
             system(f"curl {url} --output {path_to_save}")
