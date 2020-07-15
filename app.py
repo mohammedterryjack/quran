e4ec55ef2ce0b93239b968bec6cb51fed0f69846
Mohammed Terry-Jack <mohammedterryjack@gmail.com>
2020-07-15 13:48:22 +0100

    metadata included

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ from html_templates.utils import (
 )
 from data_analysis.semantic_featuriser import set_of_semantic_features_for_sentence
 ##########################################################
-# DEFAULT_TRANSLATOR = 6
-# QURAN_AUDIO = QuranAudio()
-# QURAN = QuranText()
-# with open("html_templates/quran_verse.html") as html_file:
-#     QURAN_VERSE_TEMPLATE = html_file.read()
+DEFAULT_TRANSLATOR = 6
+QURAN_AUDIO = QuranAudio()
+QURAN = QuranText()
+with open("html_templates/quran_verse.html") as html_file:
+    QURAN_VERSE_TEMPLATE = html_file.read()
 
 BIBLE_AUDIO = BibleAudio()
 BIBLE = BibleText()
@@ -81,54 +81,54 @@ def hello() -> str:
 #         previous_page_url = f"/quran/{previous_verse_key.replace(':','/')}",
 #     )
     
-# @app.route('/quran/<chapter>/<verse>')
-# def display_quranic_verse(chapter:str,verse:str) -> str:
-#     verse_key = f"{chapter}:{verse}"
-#     next_verse_key = QURAN.next_verse(verse_key)
-#     previous_verse_key = QURAN.previous_verse(verse_key)
-#     verses = QURAN.similar_verses_to_verse(verse_key, scripture=0, top_n=5)
-#     related_verses = format_and_link_verses_for_html(
-#         verses=verses[1:],
-#         verses_to_display=map(
-#             lambda verse_name:QURAN.english_translation_of_verse(
-#                 verse=verse_name,
-#                 translator=DEFAULT_TRANSLATOR
-#             )[:50],
-#             verses[1:]
-#         ),
-#         scripture="quran"
-#     )
-#     bible_verse_names = QURAN.similar_verses_to_verse(verse_key, scripture=1, top_n=5)
-#     bible_verses = format_and_link_verses_for_html(
-#         verses=bible_verse_names,
-#         verses_to_display = map(
-#             lambda verse_name:BIBLE.verse(
-#                 language_code="en",
-#                 cannon=verse_name.split(":")[0],
-#                 book=verse_name.split(":")[1],
-#                 chapter=int(verse_name.split(":")[2]),
-#                 verse=int(verse_name.split(":")[3])
-#             )[:50],
-#             bible_verse_names
-#         ),
-#         scripture="bible"
-#     )
-#     return QURAN_VERSE_TEMPLATE.format(
-#         chapter=chapter,
-#         verse=verse,
-#         chapter_name=QURAN.CHAPTER_NAMES()[int(chapter)-1],
-#         verse_audio_hafs=QURAN_AUDIO.url(verse_key,0),
-#         verse_audio_warsh=QURAN_AUDIO.url(verse_key,1),
-#         verse_in_arabic=QURAN.arabic_verse(verse_key),
-#         verses_in_english=format_sentences_to_be_hidden_html(
-#             sentences=QURAN.english_translations_of_verse(verse_key),
-#             default_displayed=DEFAULT_TRANSLATOR
-#         ),
-#         related_verses_quran=related_verses,
-#         related_verses_bible=bible_verses,
-#         next_page_url = f"/quran/{next_verse_key.replace(':','/')}",
-#         previous_page_url = f"/quran/{previous_verse_key.replace(':','/')}"
-#     )
+@app.route('/quran/<chapter>/<verse>')
+def display_quranic_verse(chapter:str,verse:str) -> str:
+    verse_key = f"{chapter}:{verse}"
+    next_verse_key = QURAN.next_verse(verse_key)
+    previous_verse_key = QURAN.previous_verse(verse_key)
+    verses = QURAN.similar_verses_to_verse(verse_key, scripture=0, top_n=5)
+    related_verses = format_and_link_verses_for_html(
+        verses=verses[1:],
+        verses_to_display=map(
+            lambda verse_name:QURAN.english_translation_of_verse(
+                verse=verse_name,
+                translator=DEFAULT_TRANSLATOR
+            )[:50],
+            verses[1:]
+        ),
+        scripture="quran"
+    )
+    bible_verse_names = QURAN.similar_verses_to_verse(verse_key, scripture=1, top_n=5)
+    bible_verses = format_and_link_verses_for_html(
+        verses=bible_verse_names,
+        verses_to_display = map(
+            lambda verse_name:BIBLE.verse(
+                language_code="en",
+                cannon=verse_name.split(":")[0],
+                book=verse_name.split(":")[1],
+                chapter=int(verse_name.split(":")[2]),
+                verse=int(verse_name.split(":")[3])
+            )[:50],
+            bible_verse_names
+        ),
+        scripture="bible"
+    )
+    return QURAN_VERSE_TEMPLATE.format(
+        chapter=chapter,
+        verse=verse,
+        chapter_name=QURAN.CHAPTER_NAMES()[int(chapter)-1],
+        verse_audio_hafs=QURAN_AUDIO.url(verse_key,0),
+        verse_audio_warsh=QURAN_AUDIO.url(verse_key,1),
+        verse_in_arabic=QURAN.arabic_verse(verse_key),
+        verses_in_english=format_sentences_to_be_hidden_html(
+            sentences=QURAN.english_translations_of_verse(verse_key),
+            default_displayed=DEFAULT_TRANSLATOR
+        ),
+        related_verses_quran=related_verses,
+        related_verses_bible=bible_verses,
+        next_page_url = f"/quran/{next_verse_key.replace(':','/')}",
+        previous_page_url = f"/quran/{previous_verse_key.replace(':','/')}"
+    )
 
 @app.route('/bible/<cannon>/<book>/<chapter>/<verse>')
 def display_bible_verse(cannon:str,book:str,chapter:str,verse:str) -> str:
